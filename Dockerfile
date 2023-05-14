e84462dcbf24c1200e1e0a2b99c471cfe0f7338e
daniel.kohav <danielkohav2@gmail.com>
2023-05-14 14:33:28 +0300

    trying deployment

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:alpine3.17
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip3 install -r requirements.txt
+
+COPY templates .
+COPY app2.py .
+
+CMD python3 app2.py
---
+++ app2.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+import boto3, json
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run()
